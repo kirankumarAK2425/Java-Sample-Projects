e29006915a7d79cc01af85e33cb6287ab6fa4a67
kirankumarak2425 <kirankumarb141@gmail.com>
2025-11-12 12:57:53 +0530

    Add files via upload

---
+++ Even_Odd.java
@@ -0,0 +1,12 @@
+package Example;
+
+public class Even_Odd {
+	public static void main(String[] args) {
+		int num=6;
+		if(num%2==0)
+			System.out.println("Even");
+		else
+			System.out.println("Odd");
+	}
+
+}
---
+++ LargestOfThreeNumbers.java
@@ -0,0 +1,20 @@
+package Example;
+
+public class LargestOfThreeNumbers {
+public static void main(String[] args) {
+	int a=10, b=20, c=15;
+	
+	if(a>b && a>c)
+		System.out.println("A is largest");
+	else if(b>a && b>c)
+		System.out.println("B is largest");
+	else
+		System.out.println("C is largest");
+	
+	
+}
+	
+	
+	
+	
+}
